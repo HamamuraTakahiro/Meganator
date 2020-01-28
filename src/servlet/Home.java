91a1c0011a43ffeff934930861091b5937ad25b3
sobe-t <sobe-tadahisa@mgt-net.com>
2020-01-28 15:50:32 +0900

    フォワードするhome.jspのパスを修正 可読性を高めるために変数を若干修正

--- src/servlet/Home.java
+++ src/servlet/Home.java
@@ -33,19 +33,17 @@ public class Home extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		/* 10個の質問をsession scopeにList<Question>型として格納。
-		 * QuestionLogic.choiceRandomQuestions()List<Question>を取得。
-		 * 変数 questions に格納。2. 1つ目の質問を出す。
-		 *  QuestioLogic.choiceRandomQuestions()を呼ぶ
+		/*
+		 *
 		 */
-		QuestionLogic qLogic = new QuestionLogic();
-		List<Question> questions = qLogic.choiceRamdomQuestions();
+		QuestionLogic questionLogic = new QuestionLogic();
+		List<Question> tenQuestions = questionLogic.choiceRamdomQuestions();
 
 		HttpSession session = request.getSession();
-		session.setAttribute("questions", questions/* 10個の質問 */);
+		session.setAttribute("tenQuestions", tenQuestions);
 
-		request.setAttribute("questionText", questions.get(0));
-		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/home.jsp");
+		request.setAttribute("questionText", tenQuestions.get(0));
+		RequestDispatcher dispatcher = request.getRequestDispatcher("home.jsp");
 		dispatcher.forward(request, response);
 
 	}
