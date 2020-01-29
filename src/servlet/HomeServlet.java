8f64ef9b8fea9eebe3af4e56155e3e430e97eeab
MGT-RD25 <MGT-RD25@MGT-RD25>
2020-01-29 15:55:42 +0900

    delete unnecessary imports.

--- src/servlet/HomeServlet.java
+++ src/servlet/HomeServlet.java
@@ -11,9 +11,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import com.sun.org.apache.bcel.internal.generic.NEW;
-
 import model.Question;
 import model.QuestionLogic;
 import model.Result;
