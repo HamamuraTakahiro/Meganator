a0c3718905283765b2ccef4e8a450c56a2204f5b
HamamuraTakahiro <hamataka.0301.mac@gmail.com>
2020-01-30 15:32:38 +0900

    create jar files for deploy(second)

---
+++ target/Meganator-0.0.1-SNAPSHOT/js/test.js
@@ -0,0 +1,36 @@
+/**
+ *
+ */
+$(function () {
+
+    $('#Hidetext1').hide();
+    $('#Hidetext2').hide();
+    $('#Hidetext3').hide();
+    $('#Hidebutton').hide();
+
+    console.log("読み込みOK！");
+
+    $('#show').click(function () {
+        console.log('クリックされました！');
+        $('#Hidetext1').show();
+    })
+
+    $('#btnHide1').click(function () {
+        console.log('クリックされました！');
+        $('#Hidetext1').hide();
+        $('#Hidetext2').show();
+    })
+
+    $('#btnHide2').click(function () {
+        console.log('クリックされました！');
+        $('#Hidetext2').hide();
+        $('#Hidetext3').show();
+
+    })
+    $('#btnHide3').click(function () {
+        console.log('クリックされました！');
+        $('#Hidetext3').hide();
+        $('#Hidebutton').show();
+    })
+
+});
